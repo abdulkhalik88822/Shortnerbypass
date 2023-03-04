722d17212e8a7f73b14a931c37537f612a2452c1
Hintpirox <120027363+Hintpirox@users.noreply.github.com>
2023-03-04 18:10:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Hintpirox/blackmen.git /blackmen
-cd /blackmen
+git clone https://github.com/Hintpirox/Shubham.git /Shubham
+cd /Shubham
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
